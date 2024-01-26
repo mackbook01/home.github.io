26ebbd8bbadd66fab59758f576bbefdc27a70015
Mack <126987107+mackbook01@users.noreply.github.com>
2024-01-26 19:40:28 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML>
+<title>Home of Madness</title>
+
+<meta content="Home of Madness" property="og:title" />
+<meta content="You will have to find out what this website is all about!" property="og:description" />
+<meta content="https://home--macbook01.repl.co/" property="og:url" />
+<meta content="https://images.macbook01.repl.co/SmurfCat.png" property="og:image" />
+<meta content="#224de3" data-react-helmet="true" name="theme-color" />
+
+<link rel="icon" href="images/SmurfCat.png">
+<hr>
+<div class="button-container">
+  <button type="button" disabled><img src="https://images.macbook01.repl.co/SmurfCat.png" width="11"
+      height="11" />Home</button>
+
+  <button onclick="window.location.href='https://commands.macbook01.repl.co/';"><img
+      src="https://images.macbook01.repl.co/windows.png" width="11" height="11" /> Windows Commands</button>
+
+  <button onclick="window.location.href='https://games.macbook01.repl.co/';"><img
+      src="https://images.macbook01.repl.co/game.png" width="11" height="11" /> Games</button>
+</div>
+<hr>
+<p>‎ </p>
+<hr>
+<html>
+
+<head>
+  <style>
+    .parent {
+      position: relative;
+      top: 0;
+      left: 0;
+    }
+
+    .image1 {
+      position: relative;
+      top: 0;
+      left: 0;
+      border: 1px solid #000000;
+    }
+  </style>
+</head>
+
+
+<body>
+  <div class="parent">
+    <center><img class="image1" style='border-radius: 20px;' src="  https://images.macbook01.repl.co/banner.png"
+        width="90%" height="0.001%" /></center>
+  </div>
+</body>
+
+<hr>
+
+
+<center>
+  <button onclick="window.location.href='https://www.youtube.com/watch?v=xvFZjo5PgG0';">Click me!</button>
+</center>
+<p></p>
+<p></p>
+<center>
+  <button onclick="myFunction1()">Secret Button?!?!</button>
+  <p id="button1"></p>
+  <script>
+    function myFunction1(button1) {
+
+      document.getElementById("button1").innerHTML = "<hr>You clicked a button congrats! 😁<hr>";
+    }
+  </script>
+</center>
+
+<center>This is basically all of the stuff here.</center>
+
+<p></p>
+<hr>
+<p></p>
+
+<section>
+  <h1>We live🌲 We Love💖 We Lie🤥</h1>
+  <p><img src="images/cat.png" width="320" height="180" /></p>
+</section>
+
+<section>
+  <h1>Monday left me broken 🥲</h1>
+  <p> <img src="images/monday.jpeg" width="320" height="180" /> </p>
+  <hr>
+</section>
+
+<p>‎ </p>
+
+
+<div class="buttons">
+
+  <div class="action_btn">
+
+
+    <button onclick="myFunction2()">Click for free robuxxx 🤑💸</button>
+    <p id="button2"></p>
+    <script>
+      function myFunction2(button2) {
+
+        document.getElementById("button2").innerHTML = "<hr>You got a virus 🦠😔 <hr>";
+      }
+    </script>
+
+    <p>‎ </p>
+    <button onclick="myFunction3()">The meaning of life!?!?</button>
+    <p id="button3"></p>
+    <script>
+      function myFunction3(button3) {
+
+        document.getElementById("button3").innerHTML = "<hr>Who knows 🤷‍♂️<hr>";
+      }
+    </script>
+
+    <p id="saved"></p>
+
+
+  </div>
+
+  <p>‎ </p>
+
+
+  <hr>
+
+  <details>
+    <summary>Open me</summary>
+    <p>Test</p>
+  </details>
+
+  <hr>
+
+  <input list="subjects">
+  <datalist id="subjects">
+    <option>Test1</option>
+    <option>Test2</option>
+    <option>Test3</option>
+  </datalist>
+
+  <hr>
+
+  <label for="skills">Website Progress </label>
+  <progress id="skills" vaule="1" max="100">
+
+    <hr>
